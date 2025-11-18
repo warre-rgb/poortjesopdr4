38b5da92c75c16aa0b231d1b1e3ce5a2e44aaf25
warre-rgb <warre@denheuvel.be>
2025-11-18 11:25:16 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,20 +1,7 @@
 basic.forever(function () {
-    if (!(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) && !(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right))) {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up) && (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) && (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)))) {
         basic.showNumber(1)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) {
-        basic.showNumber(0)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
-        basic.showNumber(0)
-    }
-})
-basic.forever(function () {
-    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+    } else {
         basic.showNumber(0)
     }
 })

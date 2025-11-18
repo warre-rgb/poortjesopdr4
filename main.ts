4f15d80d12060e5e3d9fcfb24a963db27249e1ba
warre-rgb <warre@denheuvel.be>
2025-11-18 11:13:22 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
 basic.forever(function () {
+    if (!(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) && !(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right))) {
+        basic.showNumber(1)
+    }
+})
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) {
+        basic.showNumber(0)
+    }
+})
+basic.forever(function () {
     if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) && Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
         basic.showNumber(0)
-    } else {
-        basic.showNumber(1)
+    }
+})
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+        basic.showNumber(0)
     }
 })
